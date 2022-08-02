e8bb35abc0683d53611ff5bef2796ca6c1d44e92
ilhan <ilhanilhan2021@gmail.com>
2022-08-02 18:05:05 +0300

    Java okulproje

---
+++ src/practice/practice_day12_okulProje/Runner.java
@@ -0,0 +1,9 @@
+package practice.practice_day12_okulProje;
+
+import static practice.practice_day12_okulProje.Islemler.girisPaneli;
+
+public class Runner {
+    public static void main(String[] args) {
+        girisPaneli();//static metod import edilidi
+    }
+}
