2f022e000e408237100727079438244947354d59
ilhan <ilhanilhan2021@gmail.com>
2022-08-02 17:42:08 +0300

    Java okulproje

---
+++ src/practice/practice_day12_okulProje/Islemler.java
@@ -0,0 +1,18 @@
+package practice.practice_day12_okulProje;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Islemler {
+
+    static List<Kisi> ogretmenList = new ArrayList<>();
+
+    public static void girisPaneli(){
+        System.out.println("===============================");
+        System.out.println("ÖĞRENCİ VE ÖĞRETMEN YONETİM PANELİ");
+        System.out.println("===============================");
+        System.out.println("1- OGRENCI ISLEMLERİ");
+        System.out.println("2- OGRETMEN ISLEMLERİ");
+        System.out.println("Q- CIKIS");
+    }
+}
