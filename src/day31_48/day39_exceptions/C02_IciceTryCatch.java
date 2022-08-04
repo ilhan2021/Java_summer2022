c5f36912091537e0fa0615fd32eb2f16be9f705f
ilhan <ilhanilhan2021@gmail.com>
2022-08-05 00:51:43 +0300

    Java dersleri

--- src/day31_48/day39_exceptions/C02_IciceTryCatch.java
+++
@@ -1,9 +0,0 @@
-package day31_48.day39_exceptions;
-
-import java.io.FileInputStream;
-
-public class C02_IciceTryCatch {
-    public static void main(String[] args) {
-        FileInputStream fis = new FileInputStream("src/day31_48/day39_exceptions/Test.txt");
-    }
-}
