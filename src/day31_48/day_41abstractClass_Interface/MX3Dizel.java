0f651b7235c5bcf7aa16d9cc538652157bd1fa01
ilhan <ilhanilhan2021@gmail.com>
2022-08-05 23:46:54 +0300

    Java ders not

---
+++ src/day31_48/day_41abstractClass_Interface/MX3Dizel.java
@@ -0,0 +1,9 @@
+package day31_48.day_41abstractClass_Interface;
+
+public abstract class MX3Dizel extends LX3 {
+    /*
+       Bir abstract class
+       concrete bir class'i parent edinebilir
+     */
+}
+
