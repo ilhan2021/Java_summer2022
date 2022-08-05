b1e807551614863e21d81a5fdc2ca444ca6495ed
ilhan <ilhanilhan2021@gmail.com>
2022-08-05 21:59:55 +0300

    Java ders notlari

---
+++ src/day31_48/day_41abstractClass_Interface/DAraba.java
@@ -0,0 +1,16 @@
+package day31_48.day_41abstractClass_Interface;
+
+public abstract class DAraba {
+    public static void main(String[] args) {
+        /*
+
+         */
+
+    }
+    public abstract void motor();
+    /*
+
+
+     */
+
+}
