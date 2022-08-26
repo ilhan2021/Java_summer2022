090560de027aab53b970cda29f45b8bcfe57dcad
ilhan ilhan <ilhanilhan.2021@gmail.com>
2022-08-27 00:54:58 +0300

    Java Ders Notlari

---
+++ src/practice/oop_Consept_Practice/Lessons2_Array_ArrayList.java
@@ -0,0 +1,33 @@
+package practice.oop_Consept_Practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Lessons2_Array_ArrayList {
+    public static void main(String[] args) {
+
+        String[] pazarSepeti = new String[5];
+
+        String[] pazarSepeti2 = {"elma","patates","kivi","karpuz","muz"};
+
+        List<String > listPazarSepeti = new ArrayList<>();
+        listPazarSepeti.add("elma");
+        listPazarSepeti.add("patates");
+        listPazarSepeti.remove("patates");
+
+
+        List<String> listPazarSepeti2 = Arrays.asList(pazarSepeti2);
+
+        System.out.println(listPazarSepeti2.size());
+        listPazarSepeti.add(1,"çikolata");
+
+        listPazarSepeti.set(0,"pasta");
+
+        System.out.println(listPazarSepeti2.get(0));
+
+        if(!listPazarSepeti.contains("ketcap")) listPazarSepeti.add("ketcap");
+        System.out.println(listPazarSepeti);
+
+    }
+}
