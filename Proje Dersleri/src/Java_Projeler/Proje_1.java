098b6c9d35deacfc9439903ecabe15c9a89e479a
ilhan2021 <ilhanilhan.2021@gmail.com>
2022-10-12 01:34:13 +0300

    Java Dersleri

---
+++ Proje Dersleri/src/Java_Projeler/Proje_1.java
@@ -0,0 +1,17 @@
+package Java_Projeler;
+
+public class Proje_1 {
+    public static void main(String[] args) {
+        int myAge = 8;
+        double earlyYear = 2;
+        earlyYear = earlyYear*10.5;
+        int laterYears = myAge-2;
+
+        laterYears = laterYears*4;
+        System.out.println(earlyYear);
+        System.out.println(laterYears);
+
+
+
+    }
+}
